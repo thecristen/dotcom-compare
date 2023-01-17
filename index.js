f789619af426586f99c9a479f7a23ce4c2093b36
Cristen Jones <cjones3@mbta.com>
2023-01-17 17:24:10 -0500

    more enhancements!
    - use single browser object
    - increase timeout
    - better selector for CMS images
    - nicer log messages

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ import { validateURLs } from "./lib/helpers.js";
 import compare from "./lib/compare.js";
 import generateHTML from "./lib/template.js";
 import allPaths from "./lib/pathnames.js";
+import { chromium } from "playwright";
 
 fs.emptyDirSync("diffs");
 validateURLs();
+global.browser = await chromium.launch();
 
 console.log(`🆗 Comparing ${process.env.BASE_URL} and ${process.env.TEST_URL}`);
 console.time("visual comparison");
 // const allPaths = ["/"];
 const results = await Promise.all(allPaths.flatMap(path => {
-  console.log("comparing", path);
   return [
     compare({ path, viewport: "full", baseUrl: process.env.BASE_URL, testUrl: process.env.TEST_URL }),
     compare({ path, viewport: "mobile", baseUrl: process.env.BASE_URL, testUrl: process.env.TEST_URL })
   ];
 }));
+global.browser.close();
 console.timeEnd("visual comparison");
 // console.log("results", results);
 console.time("report generation");
